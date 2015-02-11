9e9ac1e253a741497a033dad055c0be5cff5d323
Paola Garcia <garciaamezola@gmail.com>
2015-02-11 02:53:38 +0000

    using made up gene accession names and regular expressions wrote programs that would only print out the accession names that satisfied certain criteria. Remember before using modeule re to type import re

---
+++ accession_names.py
@@ -0,0 +1,46 @@
+history
+accs = ["xkn59438", "yhdck2", "eihd39d9", "chdsye847", "hedle3455", "xjhd53e", "45da", "de37dp"]
+for acc in accs:
+    if re.search(r"5", acc):
+        print("\t" + acc)
+import re
+for acc in accs:
+    if re.search(r"5", acc):
+        print("\t" + acc)
+for acc in accs:
+    if re.search(r"(d|e)", acc):
+        print("\t" + acc)
+for acc in accs:
+    if re.search(r"d.*e", acc):
+        print("\t" + acc)
+for acc in accs:
+    if re.search(r"(d.e)", acc):
+        print("\t" + acc)
+for acc in accs:
+    if re.search(r"d.*e, acc) or re.searc(r"e.*d", acc):
+        print("\t" + acc)
+for acc in accs:
+    if re.search(r"d.*e", acc) or re.searc(r"e.*d", acc):
+        print("\t" + acc)
+for acc in accs:
+    if re.search(r"d.*e", acc) or re.searc(r"e.*d", acc):
+        print("\t" + acc)
+for acc in accs:
+    if re.search(r"d.*e", acc) or re.search(r"e.*d", acc):
+        print("\t" + acc)
+for acc in accs:
+    if re.search(r"^(x|y)", acc):
+        print("\t" + acc)
+for acc in accs:
+    if re.search(r"^(x|y).*e$", acc):
+        print("\t" + acc)
+for acc in accs: if re.search(r"[0123456789]{3,100}", acc):
+for acc in accs:
+    if re.search(r"[0123456789]{3,100}", acc):
+        print("\t" + acc)
+for acc in accs:
+    if re.search(r"\d{3,}", acc):
+        print("\t" + acc)
+for acc in accs:
+    if re.search(r"d[arp]$", acc):
+        print("\t" + acc)
